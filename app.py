07bcd57cc7549c9939cd27de4d0a29a24f9b2ebc
Shivani <156783311+shivani100604@users.noreply.github.com>
2025-07-11 07:08:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ def process():
     return jsonify({"response": response})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
 
